66257d29710cf143d9b17893260a26792a749c06
Sebastian <sebas.programador@outlook.com>
2026-04-26 17:36:24 -0500

    feat: :sparkles: agrega encabezado principal

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <ion-header>
+      <ion-toolbar>
+        <ion-buttons slot="end">
+          <ion-button shape="round" @click="handleGoToFavorites">
+            <ion-icon slot="icon-only" :icon="heartOutline"></ion-icon>
+          </ion-button>
+         </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+</template>
+
+<script setup lang="ts">
+import { IonHeader, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/vue';
+import {heartOutline} from 'ionicons/icons'
+
+function handleGoToFavorites() {
+  //
+}
+</script>
