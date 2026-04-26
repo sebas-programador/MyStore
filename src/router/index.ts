7dc7535424abb16d29609c07284f2ee835cae85a
Sebastian <sebas.programador@outlook.com>
2026-04-26 17:38:29 -0500

    feat: :sparkles: agrega rutas de productos y elimina las innecesarias

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,25 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+
+import MainLayout from '@/layouts/MainLayout.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
+    redirect: '/productos'
   },
+
   {
-    path: '/tabs/',
-    component: TabsPage,
+    path: '/productos/',
+    component: MainLayout,
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        component: () => import('@/views/products/ProductsPage.vue')
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
-      },
-      {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: ':id',
+        component: () => import('@/views/products/ProductDetailPage.vue')
       }
     ]
   }
